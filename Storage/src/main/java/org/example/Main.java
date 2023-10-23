2eb5b2ae6fe9672b5cd632e882bdb593319d43f8
Vladimir Gabov <gabov.vladimir@mail.ru>
2023-10-23 13:58:59 +1000

    Написание логики в Main
    Попутно создал Jar файл

--- Storage/src/main/java/org/example/Main.java
+++ Storage/src/main/java/org/example/Main.java
@@ -1,19 +1,62 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static void main(String[] args) throws Exception {
+        if(args.length > 1)
+        {
+            throw new Exception("Many arguments!");
         }
+
+        String path = args[0];
+        IReadable readFile = new ReadDataFromFile();
+        String finallyStr =  readFile.Read(path);
+
+        List<UsefulObject> usefulObjects = GetDataFromString(finallyStr);
+
+        Map<Integer,UsefulObject> dictionary = SetDataFromDictionary(usefulObjects);
+
+        RequestService service = new RequestService(dictionary);
+
+        service.RunService();
+    }
+
+    private  static  List<UsefulObject> GetDataFromString(String finallyStr)
+    {
+        List<UsefulObject> usefulObjects = new ArrayList<UsefulObject>();
+
+        String[] str = finallyStr.split("\n");
+        for (String s : str) {
+            String[] value = s.split(",");
+
+            UsefulObject newObj = new UsefulObject
+                    (
+                            Integer.parseInt(value[0]),
+                            value[1],
+                            value[2],
+                            value[3]
+                    );
+
+            usefulObjects.add(newObj);
+        }
+        return usefulObjects;
+    }
+
+    private  static Map<Integer,UsefulObject> SetDataFromDictionary(List<UsefulObject> datas)
+    {
+        Map<Integer,UsefulObject> dictionary = new HashMap<Integer,UsefulObject>();
+
+        for (UsefulObject data : datas)
+        {
+            dictionary.put(data.ID, data);
+        }
+
+        return  dictionary;
     }
 }
