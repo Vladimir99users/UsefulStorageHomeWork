7ca3b0d123dab4bad8e82678604f70bee13141ff
Vladimir Gabov <gabov.vladimir@mail.ru>
2023-10-25 13:09:30 +1000

    переписал Main класс,
    Удалил не нужное заполнение словаря, перенёс все в RequestService, чуток подчистил данные

--- Storage/src/main/java/org/example/Main.java
+++ Storage/src/main/java/org/example/Main.java
@@ -5,9 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class Main
+{
     public static void main(String[] args) throws Exception {
         if(args.length > 1)
         {
@@ -15,16 +14,20 @@ public class Main {
         }
 
         String path = args[0];
-        IReadable readFile = new ReadDataFromFile();
-        String finallyStr =  readFile.Read(path);
+        Readable readFile = new ReadDataFromFile();
+        String finallyStr =  readFile.readData(path);
 
         List<UsefulObject> usefulObjects = GetDataFromString(finallyStr);
 
-        Map<Integer,UsefulObject> dictionary = SetDataFromDictionary(usefulObjects);
+        RequestService requestService = new RequestService();
 
-        RequestService service = new RequestService(dictionary);
 
-        service.RunService();
+        SetDataFromRequestService(requestService, usefulObjects);
+
+
+        DisplayService displayService = new DisplayService(requestService);
+
+        displayService.RunService();
     }
 
     private  static  List<UsefulObject> GetDataFromString(String finallyStr)
@@ -37,10 +40,9 @@ public class Main {
 
             UsefulObject newObj = new UsefulObject
                     (
-                            Integer.parseInt(value[0]),
+                            value[0],
                             value[1],
-                            value[2],
-                            value[3]
+                            value[2]
                     );
 
             usefulObjects.add(newObj);
@@ -48,15 +50,11 @@ public class Main {
         return usefulObjects;
     }
 
-    private  static Map<Integer,UsefulObject> SetDataFromDictionary(List<UsefulObject> datas)
+    private  static void SetDataFromRequestService(RequestService requestService, List<UsefulObject> datas)
     {
-        Map<Integer,UsefulObject> dictionary = new HashMap<Integer,UsefulObject>();
-
         for (UsefulObject data : datas)
         {
-            dictionary.put(data.ID, data);
+            requestService.AddUsefulObject(data);
         }
-
-        return  dictionary;
     }
 }
