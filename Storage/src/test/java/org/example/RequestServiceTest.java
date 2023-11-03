a61328c6bd35594b03b548aa3485da78cfc32a8d
Vladimir Gabov <gabov.vladimir@mail.ru>
2023-11-03 18:19:43 +1000

    Переработал тесты.
    Добавил метод setup, что бы установить все зависимости.
    А так же считать данные.
    Переработал методы.

--- Storage/src/test/java/org/example/RequestServiceTest.java
+++ Storage/src/test/java/org/example/RequestServiceTest.java
@@ -1,74 +1,98 @@
 package org.example;
 
-import org.junit.Test;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
-import static org.junit.Assert.*;
-import static org.junit.Assert.assertNotNull;
 
 public class RequestServiceTest
 {
+
+    private  RequestService requestService;
+
+    @BeforeEach
+    public  void setup()
+    {
+        Requestable request = new Requests();
+        requestService = new RequestService(request);
+
+        try {
+            requestService.readJsonFile(getPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
     @Test
     public  void addToStorageUsefulObjects()
     {
-        //Тест на проверку добавления объекта.
-        RequestService requestService = new RequestService(new Requests());
+        //Тест на проверку добавления объекта
 
-        UsefulObject objExpected = new UsefulObject("Drop down", "Отключиться", "https://yandex.ru");
+        //Arrange
+        String nameTest = "kkk";
+        String descriptions = "Enable";
+        String link = "https://yandex.ru";
+        long idTest = Long.parseLong(String.format("%s%d", nameTest.hashCode(), descriptions.length()));
+
+        //act
+        UsefulObject objExpected = new UsefulObject(idTest, nameTest, descriptions, link);
 
         requestService.addUsefulObject(objExpected);
 
-        assertEquals(objExpected, requestService.getUsefulObjectByID(objExpected.getId()));
+        UsefulObject objActual = requestService.getUsefulObjectByID(objExpected.getId());
+
+        //Assert
+        Assertions.assertNotNull(objActual);
+        Assertions.assertEquals(objExpected, objActual );
     }
     @Test
     public void correctIDTest()
     {
         // Тест на проверку ID - ID это хэш код имени
-        String checkName = "Fedor";
-        String desctiption = "age 17";
+        String checkName = "Test";
+        String desctiption = "Enable";
         long expectedID = Long.parseLong(String.format("%s%d", checkName.hashCode(), desctiption.length() ));
-
-        RequestService requestService = new RequestService(new Requests());
-
-        UsefulObject obj = new UsefulObject(checkName, desctiption, "https://Fedor.com");
-        requestService.addUsefulObject(obj);
 
         UsefulObject newObj = requestService.getUsefulObjectByID(expectedID);
 
-        assertNotNull(newObj);
-        assertEquals(expectedID, newObj.getId());
+        Assertions.assertNotNull(newObj);
+        Assertions.assertEquals(expectedID, newObj.getId());
     }
 
     @Test
     public void searchUsefulObjectToNameTest()
     {
-        //Проверка на правильность поиска имени, суть в том, что мы должны находить массив объектов, с одинаковым именем, а это значит, что если первый элемент проходит, то и остальные тоже.
-        String expectedName = "Fedor";
+       //Проверка на правильность поиска имени, суть в том, что мы должны находить массив объектов, с одинаковым именем, а это значит, что если первый элемент проходит, то и остальные тоже.
+        String expectedName = "Test";
 
-        UsefulObject usefulObjFirst = new UsefulObject(expectedName, "age 17", "https://Fedor.com");
-        UsefulObject usefulObjSecond = new UsefulObject(expectedName, "age 182", "https://Fedor228.com");
-        UsefulObject usefulObjectThird = new UsefulObject("Alina", "None", "https://Alina1997@gmail.com");
-
-        RequestService requestService = new RequestService(new Requests());
-
-        requestService.addUsefulObject(usefulObjFirst);
-        requestService.addUsefulObject(usefulObjSecond);
-        requestService.addUsefulObject(usefulObjectThird);
 
         List<UsefulObject> newObjs = requestService.getUsefulObjectsByName(expectedName);
 
-        assertNotNull(newObjs);
-        assertEquals(expectedName, newObjs.get(0).getName());
-        assertEquals(expectedName, newObjs.get(1).getName());
+        Assertions.assertNotNull(newObjs);
+
+        for (UsefulObject tmpObj : newObjs)
+        {
+            Assertions.assertEquals(expectedName, tmpObj.getName());
+        }
 
     }
 
-    private String getPath()
+    private String getPath() throws IOException
     {
-        String resourceName = "TestFile.txt";
+        String resourceName = "TestFile.json";
 
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource(resourceName).getFile());
