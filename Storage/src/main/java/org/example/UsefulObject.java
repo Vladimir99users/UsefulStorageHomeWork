202a5ef32729708f0ed1a0eab920cca3b08e7a38
Vladimir Gabov <gabov.vladimir@mail.ru>
2023-11-03 16:06:32 +1000

    Переработал скрипт  через Lombok

--- Storage/src/main/java/org/example/UsefulObject.java
+++ Storage/src/main/java/org/example/UsefulObject.java
@@ -1,43 +1,18 @@
 package org.example;
 
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class UsefulObject
 {
     private long id;
-    private  String name;
-    private  String description;
-    private  String url;
+    private String name;
+    private String description;
+    private String url;
 
-    public UsefulObject(String name, String description, String uRL )
-    {
-        id =  Long.parseLong(String.format("%s%d", name.hashCode(), description.length()));
-        this.name = name;
-        this.description = description;
-        url = uRL;
-    }
-
-    public long getId()
-    {
-        return id;
-    }
-
-    public String getName()
-    {
-        return name;
-    }
-
-    public String getDescription()
-    {
-        return description;
-    }
-
-    public String getURL()
-    {
-        return url;
-    }
-    
-    public String getStringData()
-    {
-       return String.format("ID %d , Name %s , Description %s, URL %s ", id, name, description, url);
-    }
 }
