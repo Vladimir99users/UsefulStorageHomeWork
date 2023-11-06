8418c06929eecaf65e6dd356f78ad3520db596ff
Vladimir Gabov <gabov.vladimir@mail.ru>
2023-11-06 14:20:26 +1000

    Красивый main

--- Storage/src/main/java/org/example/Main.java
+++ Storage/src/main/java/org/example/Main.java
@@ -6,33 +6,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.beans.factory.annotation.Value;
+
+import java.util.Map;
+
 @SpringBootApplication
 public class Main
 {
-    @Autowired
-    private RequestService requestService;
-
-    @Autowired
-    private  DisplayService displayService;
-
-    @Value("${input.data.path}")
-    private String inputDataPath;
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args).close();
     }
 
-
+    @Value("${input.data.path}")
+    private String inputDataPath;
     @Bean
     public CommandLineRunner run() {
-
         return args -> {
-            System.out.println("Hello world!");
-            requestService.readJsonFile(inputDataPath);
-            displayService.runService();
+
+            CommandLiner liner = new CommandLiner(inputDataPath);
+            liner.Initialize();
+            liner.run(args);
         };
     }
-
-
-
-
 }
