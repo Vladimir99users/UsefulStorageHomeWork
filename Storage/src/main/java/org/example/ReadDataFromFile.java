81c1de1d1c6a313d8dab5435c3f7dc51300cde7c
Vladimir Gabov <gabov.vladimir@mail.ru>
2023-11-06 14:21:48 +1000

    ReadDataFromFile
    Заменил readTree -> на readValue
    стало почище, но словил много косяков) но всё исправил)
    ID у меня через hach просчитывается, поэтому всё стало чуток короче, но не на много

--- Storage/src/main/java/org/example/ReadDataFromFile.java
+++ Storage/src/main/java/org/example/ReadDataFromFile.java
@@ -1,52 +1,52 @@
 package org.example;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.Serial;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-@Component
 public  class  ReadDataFromFile implements Readable
 {
-
-
     @Override
-    public Map<Long, UsefulObject> readData(String path) {
+    @Bean
+    public Map<Long, UsefulObject> readData(String path)
+    {
         Map<Long, UsefulObject> datas = new HashMap<>();
+
         ObjectMapper mapper = new ObjectMapper();
-        System.out.println(path);
 
         File file = new File(path);
-        JsonNode nodes = null;
 
         try
         {
-            nodes = mapper.readTree(file);
+            List<UsefulObject> usefulObjects = mapper.readValue(file, new TypeReference<>() {});
+
+            for (UsefulObject usefulObject : usefulObjects)
+            {
+                String name = usefulObject.getName();
+                String description = usefulObject.getDescription();
+                long id =  Long.parseLong(String.format("%s%d", name.hashCode(), description.length()));
+
+                usefulObject.setId(id);
+
+                datas.put(id, usefulObject);
+            }
         }
         catch(IOException e)
         {
             System.out.println(e);
+            return  new HashMap<>();
         }
 
-
-        for(JsonNode node : nodes)
-        {
-            long id = 0;
-            String name = node.get("name").asText();
-            String description = node.get("description").asText();
-            String link = node.get("link").asText();
-
-            id =  Long.parseLong(String.format("%s%d", name.hashCode(), description.length()));
-            System.out.println(id);
-            UsefulObject obj = new UsefulObject(id,name,description,link);
-            datas.put(id,obj);
-        }
 
         return datas;
     }
