6940e2739e363d135edff6e8de8388a7fec1929c
Vladimir Gabov <gabov.vladimir@mail.ru>
2023-12-06 00:06:47 +1000

    Добавил Swagger проверенно, что всё работает.

--- Storage/src/main/java/org/example/Main.java
+++ Storage/src/main/java/org/example/Main.java
@@ -1,11 +1,20 @@
 package org.example;
 
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.beans.factory.annotation.Value;
+
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info
+                (
+                        title = "Hello World"
+                )
+)
+@Hidden
 public class Main
 {
     public static void main(String[] args) {
--- Storage/src/main/java/org/example/RestControll.java
+++ Storage/src/main/java/org/example/RestControll.java
@@ -8,49 +8,47 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping(path = "api/v1/usefulObjects")
+@RequestMapping("/")
 public class RestControll
 {
 
     private final RequestService requestService;
     private final Readable readable;
-    @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public void addNewUsefulObject(@RequestBody UsefulObject obj)
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public UsefulObject addNewUsefulObject(@RequestBody UsefulObject obj)
     {
         requestService.addUsefulObject(obj);
+        return obj;
     }
-    @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
-    public void deletedUsefulObject(UsefulObject obj)
+    @RequestMapping(value = "delete", method = RequestMethod.DELETE)
+    public void deletedUsefulObject(@RequestBody UsefulObject obj)
     {
         requestService.deletedUsefulObject(obj);
     }
 
-    @RequestMapping(value = "/GetID", method = RequestMethod.GET)
-    public UsefulObject getUsefulObjectByID(@RequestBody Long id)
+    @RequestMapping(value = "{id}", method = RequestMethod.GET)
+    public UsefulObject getUsefulObjectByID(@PathVariable Long id)
     {
         return requestService.getUsefulObjectByID(id);
     }
 
-    @RequestMapping(value = "/GetNames", method = RequestMethod.GET)
-    public List<UsefulObject> getUsefulObjectByName(String name)
+    @RequestMapping(value = "Names/{name}", method = RequestMethod.GET)
+    public List<UsefulObject> getUsefulObjectByName(@PathVariable String name)
     {
         return requestService.getUsefulObjectsByName(name);
     }
-    @RequestMapping(value = "/update", method = RequestMethod.PUT)
-    public void updateUsefulObject(Long oldIDObject,
-                                      UsefulObject newObj)
+    @RequestMapping(value = "update/{oldIDObject}", method = RequestMethod.PUT)
+    public void updateUsefulObject(@PathVariable Long oldIDObject,
+                                   @RequestBody UsefulObject newObj)
     {
-       UsefulObject updateObj = requestService.getUsefulObjectByID(oldIDObject);
-
-        if(!requestService.deletedUsefulObject(updateObj))
-            return;
+        UsefulObject updateObj = requestService.getUsefulObjectByID(oldIDObject);
+        requestService.deletedUsefulObject(updateObj);
 
         updateObj.setName(newObj.getName());
         updateObj.setLink(newObj.getLink());
         updateObj.setDescription(newObj.getDescription());
 
-        if(!requestService.addUsefulObject(updateObj))
-            return;
+        requestService.addUsefulObject(updateObj);
     }
 
 }
